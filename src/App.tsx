0199c42e55618b2350b19169f0fdb4fdd3f84639
joaquin telleria <joaquintelleria@gmail.com>
2023-07-12 15:05:48 +0200

    tested badges

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import {useEffect, useState} from "react";
 import "trmd3components/trmd3.css";
-import Button from "trmd3components/Button";
-import HorizontalCard from "trmd3components/HorizontalCard";
 import {getPreferredScheme, toggleTheme} from "trmd3components/Themeing";
+import BottomAppBar from "trmd3components/BottomAppBar";
+import TopAppBar from "trmd3components/TopAppBar";
+import Badge from "trmd3components/Badge";
+import Button from "trmd3components/Button";
+import Component from "trmd3components/Component";
 
 function App() {
 	const [_theme, setTheme] = useState(
@@ -12,6 +15,18 @@ function App() {
 	useEffect(() => {
 		localStorage.setItem("theme", _theme);
 		document.body.className = "body-" + _theme;
+
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.code === "Space") {
+				handleToggleTheme();
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
 	}, [_theme]);
 
 	const handleToggleTheme = (): void => {
@@ -20,8 +35,15 @@ function App() {
 
 	return (
 		<>
-			<Button onClick={handleToggleTheme}>Toggle Theme</Button>
-			<HorizontalCard></HorizontalCard>
+			<Component>
+				<Badge
+					configuration={"multiple-digits"}
+					anchor="top-right"
+					xOffset={-2.5}
+					yOffset={-0.6}>
+					123
+				</Badge>
+			</Component>
 		</>
 	);
 }
